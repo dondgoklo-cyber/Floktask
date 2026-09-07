79cad5623e98f352f2155dce8ddb92cc873cd578
dondgoklo-cyber <dondgoklo@gmail.com>
2026-09-08 00:18:30 +0300

    fix: исправлен разрыв слова implementation в build.gradle.kts + версия 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,7 @@ android {
 dependencies {
     implementation(libs.kotlin.stdlib)
     implementation(libs.kotlinx.coroutines.android)
-    imple
-mentation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
